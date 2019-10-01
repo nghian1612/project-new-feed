73a172c3c884c954f8a0471ac7cbeaf165bfae01
Nghĩa Nguyễn <pro@pros-MacBook-Pro.local>
2019-10-01 20:36:41 +0700

    home

---
+++ home/src/assets/js/custom.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 30,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 2
+        },
+        768: {
+            items: 3
+        }
+    }
+})
