4dcc8974abc83114236d05d33f99a1a5a0331cfa
hieutuong <hieutruong753@gmail.com>
2019-10-19 16:41:34 +0700

    cread show hide password,update css

--- template social/js/script.js
+++ template social/js/script.js
@@ -303,7 +303,16 @@ $(".user-info").on("click", function(){$("#users").hide();
 
     });
 
+    $(".toggle-password").click(function() {
 
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("toggle"));
+        if (input.attr("type") == "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+      });
 
 
 
